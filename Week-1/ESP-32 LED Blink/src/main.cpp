82a6e37d1ea18dfe4b541c060111338505c811b2
Hashir <hqadeer282@gmail.com>
2026-07-08 12:41:43 +0500

    Add initial project files for ESP-32 LED Blink example

---
+++ Week-1/ESP-32 LED Blink/src/main.cpp
@@ -0,0 +1,13 @@
+#include<Arduino.h>
+
+void setup() {
+  pinMode(4, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(4, HIGH);
+  delay(1000);
+  digitalWrite(4, LOW);
+  delay(1000);
+}
